fb4b67fb6978b34a0466928185a24b47e69b7d06
alifrashid00 <asiforrashid@iut-dhaka.edu>
2024-10-17 12:03:26 +0600

    .

--- src/pages/home/AdminHome.jsx
+++ src/pages/home/AdminHome.jsx
@@ -5,7 +5,7 @@ const AdminHome= () => {
     return <div style={{
         padding: '50px 0px 0px 370px'
     }}>
-        <AdminSidebar />
+        <AdminSidebar/>
         <Outlet />
 
     </div>;
