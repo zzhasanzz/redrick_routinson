88b1ca2f5395d096d9f7c17154f80905d3f0aa63
Shufan Shahi <shufanshahi@gmail.com>
2025-03-15 11:47:09 +0600

    KEy problem

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { collection, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVAZFRD3LdV3ZBFWJQozlVOWaRkEoEDxM",
-  authDomain: "fir-6561e.firebaseapp.com",
-  projectId: "fir-6561e",
-  storageBucket: "fir-6561e.firebasestorage.app",
-  messagingSenderId: "982148730376",
-  appId: "1:982148730376:web:3476ef0855d3259ad2cbbd",
-  measurementId: "G-X4JS0HCEH5"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 }; 
 
 
